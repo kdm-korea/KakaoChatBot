493ff103af33d0c8a769ab8949d96b20a0e30b06
kdm-korea <kdm_korea@naver.com>
2018-05-16 18:18:06 +0900

    Feat: 형태소 분석을 통한 응답 제작
    카톡서버의 Qos가 5초라서 Timeout 에러가 많이 나왔으며
    
    해당하는 에러를 잡기 위해 별도 함수를 제작하여 1.5초 이내 미응답 시 종료 콜백을 보내도록 제작

--- Nodejs/Crawling.js
+++ Nodejs/Crawling.js
@@ -2,7 +2,14 @@ exports.questRes = function(text,callback){
     var openApiURL = 'http://aiopen.etri.re.kr:8000/WiseQAnal';
     var access_key = 'accb1e07-ed73-4eb0-9f1f-7b39da8d0ab0';
 
-     var requestJson = {
+    setTimeout(latereply, 1500);
+    console.log("1");
+    function latereply(){
+        callback('검색 시간이 너무 오래 걸려요 :(');
+        console.log("2");
+    }
+    
+    var requestJson = {
         'access_key': access_key,
         'argument': {
             'text': text
@@ -15,9 +22,17 @@ exports.questRes = function(text,callback){
         body: JSON.stringify(requestJson),
         headers: {'Content-Type':'application/json; charset=UTF-8'}
     };
-    
+
     request.post(options, function (err, response, body) {
-        var acccountObj = JSON.parse(body);
+        try{
+            var acccountObj = JSON.parse(body);
+            var value = acccountObj.return_object.orgQInfo.orgQUnit.vTitles[0].vEntityInfo[0].strExplain;
+            callback(value);
             
+        }catch (e){
+            callback("ERROR");
+        }
+
     }); 
 };
+
--- Nodejs/Main.js
+++ Nodejs/Main.js
@@ -1,21 +1,41 @@
-//express, https Module use
-var express = require('express');
-var http = require('http');
-var bodyParser = require('body-parser');
-
+//search Module use
+var searchRes = require('./Crawling.js');
 //weather Module use
 var weatherRss = require('./Weather.js');
+//userres Module use
+//var userRes = require('./User.js');
 
-var app = express();
-
-//body-parser Middleware use
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-
-app.get('/keyboard', function(req, res){
-    //send data
-   
-        switch(msg){
+app.post('/message', function(req, res){
+    //Client Write Msg
+    var msg = req.body.content;
+    //Res Message
+    var send = {};
+    
+//    DB check
+//    DB delete space
+    var compare = msg.replace(/(\s*)/g,"");
+    console.log(compare);
+//    userRes.userRes(compare, function(values){
+//       if(values != 'ERROR'){
+//           send = {
+//               'message':{
+//                    'text': values
+//                }
+//           }
+//           res.json(send);
+//       }
+//    });
+//     setTimeout(latereply, 3000);
+//    function latereply(){
+//        send = {
+//                'message':{
+//                    'text': '검색 시간이 너무 오래 걸려요 :('
+//                }
+//        }
+//        res.json(send);
+//    }
+    
+    switch(msg){
         case '안녕':
 //            clearTimeout(setTimeout(latereply,3000));
             send = {
@@ -25,7 +45,16 @@ app.get('/keyboard', function(req, res){
             }
             res.json(send);
             break;
-         case '날씨':
+        case '사과':
+//            clearTimeout(setTimeout(latereply,3000));
+            send = {
+                'message':{
+                    'text': '맛있다.'
+                }
+            }
+            res.json(send);
+            break;
+        case '날씨':
 //            clearTimeout(setTimeout(latereply,3000));
             weatherRss.weatherRes(200, function(values){ //Weather Crawling
                 send = {
@@ -36,7 +65,17 @@ app.get('/keyboard', function(req, res){
                 res.json(send);
             });
             break;
-         default:
+        case '안알려줄래요': //DB Not Using
+//            clearTimeout(setTimeout(latereply,3000));
+            send = {
+                'message':{
+                    'text' : '다음에는 알려주세요! :)'
+                }
+            }
+            res.json(send);
+            break;
+        case '알려줄래요': //DB Using
+//            clearTimeout(setTimeout(latereply,3000));
             send = {
                 'message':{
                     'text' : '아직 준비가 안되었어요 :('
@@ -44,25 +83,34 @@ app.get('/keyboard', function(req, res){
             }
             res.json(send);
             break;
-        }
-    //json syntex res
-    res.json(data);
-});
-
-app.post('/message', function(req, res){
-    //Client Write Msg
-    var msg = req.body.content;
-    //Res Message
-    var send = {};
-    
-    send = {
-                'message':{
-                    'text' : '아직 준비가 안되었어요 :('
+        default:
+            searchRes.questRes(msg,function(values){ //Google Search
+                if(values == 'ERROR'){
+//                    clearTimeout(setTimeout(latereply,3000));
+                    values = '가르쳐 주실래요?';
+                    send = {
+                        'message':{
+                            'text': values
+                        },
+                        keyboard:{
+                            'type':'buttons',
+                                'buttons':['알려줄래요','안알려줄래요']
+                        }
+                    }
+                    res.json(send);    
+                }else{
+//                    clearTimeout(setTimeout(latereply,3000));
+                    send = {
+                    'message':{
+                        'text': values
+                        }
+                    }
+                    res.json(send);    
                 }
-            }
-            res.json(send);
+            });
+            break;
+    }
 });
-
 
 http.createServer(app).listen(9111, function(){
     console.log('실행중');
