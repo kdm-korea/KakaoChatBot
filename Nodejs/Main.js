1c7423a70c73b9a3b7b8587f1f2af58d445eb4a1
Kim Dong Min <kdm_korea@naver.com>
2018-11-28 21:51:47 +0900

    Post: Modified lamda

--- Nodejs/Main.js
+++ Nodejs/Main.js
@@ -9,17 +9,15 @@ var weatherRss = require('./Weather.js');
 //userres Module use
 //var userRes = require('./User.js');
 
-app.get(()=>{'/keyboard', example
-    res.json(send); 
-});
-
-var example = function(req,res){
+app.get('/keyboard',(req,res)=>{
     var send={};
     
     send.message={
         text:'안녕하세요'
     };
-};
+    res.json(send); 
+});
+
 // app.get('/keyboard',function(req,res){
 //     var send={};
     
@@ -30,11 +28,26 @@ var example = function(req,res){
 //     res.json(send);
 // });
 
-app.post('/message', function(req, res){
+var sendTxt = (sendMsg, send)=>{
+    var send = {};
+    send.message={
+        text: sendMsg
+    };
+    return send;
+}
+
+var sendBtn = (btn1, btn2, send)=>{
+    var send = {};
+    send.keyboard={
+        type:buttons,
+        buttons:[btn1, btn2]
+    };
+    return send;
+}
+
+app.post('/message', (req, res)=>{
     //Client Write Msg
     var msg = req.body.content;
-    //Res Message
-    var send = {};
     
 //    DB check
 //    DB delete space
@@ -43,70 +56,36 @@ app.post('/message', function(req, res){
 
     switch(msg){
         case '안녕':
-//            clearTimeout(setTimeout(latereply,3000));
-           
-            send.message={
-                text:'안녕하세요'
-            };
-            res.json(send);
+            res.json(sendTxt('안녕하세요'),{});
             break;
         case '사과':
-//            clearTimeout(setTimeout(latereply,3000));
-            send.message={
-                text:'맛있다'
-            };
-            res.json(send);
+            res.json(sendTxt('맛있다'),{});
             break;
         case '날씨':
-//            clearTimeout(setTimeout(latereply,3000));
-            weatherRss.weatherRes(200, function(values){ //Weather Crawling
-                send.message={
-                    text: values
-                };
-                res.json(send);
+            weatherRss.weatherRes(200, (values)=>{ //Weather Crawling
+                res.json(sendTxt(values),{});
             });
             break;
         case '안알려줄래요': //DB Not Using
-//            clearTimeout(setTimeout(latereply,3000));
-            send.message={
-                text: '다음에는 알려주세요! :)'
-            };
-            res.json(send);
+            res.json(sendTxt('다음에는 알려주세요! :)'),{});
             break;
         case '알려줄래요': //DB Using
-//            clearTimeout(setTimeout(latereply,3000));
-            send.message={
-                text= '아직 준비가 안되었어요 :('
-            };
-            res.json(send);
+            res.json(sendTxt('아직 준비가 안되었어요 :('),{});
             break;
         default:
-            searchRes.questRes(msg,function(values){ //Google Search
+            searchRes.questRes(msg,values=>{ //Google Search
                 if(values == 'ERROR'){
-//                    clearTimeout(setTimeout(latereply,3000));
-                    values = '가르쳐 주실래요?';
-                    send.message={
-                        text:values
-                    };
-                    send.keyboard={
-                        type:buttons,
-                        buttons:['알려줄래요','안알려줄래요']
-                    };
-
-                    res.json(send);    
-                }else{
-//                    clearTimeout(setTimeout(latereply,3000));
-                    send.message={
-                        text: values
-                    };
-
-                    res.json(send);
+                    res.json(sendBtn('알려줄래요','안알려줄래요',sendTxt('가르쳐 주실래요?'),{}));
+                }
+                else{
+                    res.json(examMsg(values));
                 }
             });
             break;
     }
 });
+// send 객체를 만들어주는 함수
 
 http.createServer(app).listen(9111, function(){
     console.log('실행중');
-});
+});//형식검사
